b7b2fb4a6dcb26e0cb3335d5f53d04398fcbb8e1
Omar Rendon <omarcr.96@gmail.com>
2024-09-11 01:32:41 -0600

    Add: PUT method to updat an espesific todo and validations added

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,6 @@
 import prisma from "@/app/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
+import * as yup from "yup";
 
 interface Segments {
   params: {
@@ -20,4 +21,37 @@ export async function GET(request: Request, { params }: Segments) {
     message: "Success",
     data: todo,
   });
+}
+
+const putSchema = yup.object({
+  complete: yup.boolean().optional(),
+  description: yup.string().optional(),
+});
+
+export async function PUT(request: Request, { params }: Segments) {
+  const { id } = params;
+
+  const todo = await prisma.todo.findFirst({ where: { id } });
+
+  if (!todo) {
+    return NextResponse.json({ message: "Todo not found." }, { status: 404 });
+  }
+
+  try {
+    const { complete, description } = await putSchema.validate(
+      await request.json()
+    );
+
+    const updatedTodo = await prisma.todo.update({
+      where: { id },
+      data: { complete, description },
+    });
+
+    return NextResponse.json({
+      message: "Success",
+      data: updatedTodo,
+    });
+  } catch (error) {
+    return NextResponse.json({ messsage: "Invalid data" }, { status: 400 });
+  }
 }
