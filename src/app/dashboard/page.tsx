af4dede79cad73beffd37b24ddd8239d83bd62ae
Omar Rendon <omarcr.96@gmail.com>
2024-10-02 00:01:44 -0600

    authentication and configutaritaion with NextAuth added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import WidgetItem from "@/components/WidgetItem";
 export default function DashboardPage() {
   return (
     <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-      <WidgetItem />
+      {/* <WidgetItem /> */}
     </div>
   );
 }
