f5a871e4a82dcc7be479f0112f6408725a42645e
Omar Rendon <omarcr.96@gmail.com>
2024-10-01 22:56:51 -0600

    Total to pay component added

--- src/app/dashboard/cart/page.tsx
+++ src/app/dashboard/cart/page.tsx
@@ -1,6 +1,7 @@
 import { cookies } from "next/headers";
 import { products, type Product } from "@/products/data/products";
 import { ItemCard } from "@/shopping-cart";
+import WidgetItem from "@/components/WidgetItem";
 export const metadata = {
   title: "Products Cart",
   description: "SEO Title",
@@ -31,6 +32,11 @@ export default function Cartpage() {
   };
   const productsInCart = getProductsInCart(cart);
 
+  const totalToPay = productsInCart.reduce(
+    (prev, current) => current.product.price * current.quantity + prev,
+    0
+  );
+
   return (
     <div>
       <h1 className="text-5xl">Products in the cart</h1>
@@ -41,6 +47,19 @@ export default function Cartpage() {
           {productsInCart.map(({ product, quantity }) => (
             <ItemCard key={product.id} product={product} quantity={quantity} />
           ))}
+        </div>
+
+        <div className="flex flex-col w-full sm:4/12">
+          <WidgetItem title="Total to Pay">
+            <div className="mt-2 flex justify-center gap-4">
+              <h3 className="text-3xl font-bold text-gray-7000">
+                ${(totalToPay * 1.15).toFixed(2)}
+              </h3>
+            </div>
+            <span className="font-bold text-center text-gray-500">
+              Taxes 15%: ${(totalToPay * 0.15).toFixed(2)}
+            </span>
+          </WidgetItem>
         </div>
       </div>
     </div>
