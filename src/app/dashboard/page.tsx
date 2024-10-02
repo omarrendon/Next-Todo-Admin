00838471c8d5c991d2e717df220b6200ad982290
Omar Rendon <omarcr.96@gmail.com>
2024-10-02 01:30:02 -0600

    authentication with github

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import WidgetItem from "@/components/WidgetItem";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
   return (
-    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-      {/* <WidgetItem /> */}
+    <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 ">
+      <WidgetItem title="Conected user Server Side">
+        <div className="flex flex-cols">
+          <span>{JSON.stringify(session.user)}</span>
+        </div>
+      </WidgetItem>
     </div>
   );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,6 +10,8 @@ import {
   IoListOutline,
   IoStorefrontOutline,
 } from "react-icons/io5";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 const menuItems = [
   {
@@ -39,7 +41,8 @@ const menuItems = [
   },
 ];
 
-export default function Sidebar() {
+export default async function Sidebar() {
+  const session = await getServerSession(authOptions);
   return (
     <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
       <div>
@@ -59,14 +62,18 @@ export default function Sidebar() {
 
         <div className="mt-8 text-center">
           <Image
-            src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
+            src={`${
+              session?.user?.image
+                ? session?.user?.image
+                : "https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
+            } `}
             alt=""
             className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
             width={150}
             height={150}
           />
           <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">
-            Cynthia J. Watts
+            {session?.user?.name ? session?.user?.name : "Cynthia J. Watts"}
           </h5>
           <span className="hidden text-gray-400 lg:block">Admin</span>
         </div>
