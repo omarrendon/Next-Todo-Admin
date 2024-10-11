0acc7811fbcacfa05524a3247b72ee2204aa19bf
Omar Rendon <omarcr.96@gmail.com>
2024-10-10 23:24:27 -0600

    sing in with email and password were created and password column in user table added

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,8 +2,11 @@ import prisma from "@/app/lib/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+
+import { signInEmailPassword } from "@/auth/actions/auth-actions";
 
 // export const authOptions
 
@@ -20,6 +23,32 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "user@google.com",
+        },
+        password: { label: "Password", type: "password", placeholder: "***" },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = await signInEmailPassword(
+          credentials!.email,
+          credentials!.password
+        );
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        }
+
+        return null;
+      },
+    }),
+
     // ...add more providers here
   ],
 
